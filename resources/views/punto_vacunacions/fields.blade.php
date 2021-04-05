5704496720636b3dcd3d0e0dc4ec614e5a7ed1fd
Josimar Jimenez <josimarjimenez86@gmail.com>
2021-04-05 15:43:12 -0500

    Cambio en formularios

--- resources/views/punto_vacunacions/fields.blade.php
+++ resources/views/punto_vacunacions/fields.blade.php
@@ -9,3 +9,9 @@
     {!! Form::label('unicodigo', 'Unicódigo:') !!}
     {!! Form::text('unicodigo', null, ['class' => 'form-control']) !!}
 </div>a
+
+<!-- Nombre Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('establecimiento', 'Establecimiento:') !!}
+    {!! Form::text('establecimiento', null, ['class' => 'form-control']) !!}
+</div>a
